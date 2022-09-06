3ada18561cb221d9e4f2c62eac730656a5f93391
Pinterics David <55298934+pinterid@users.noreply.github.com>
2022-09-06 12:24:47 +0000

    Prettify Code
    The code has been cleaned up.

--- src/log.ts
+++ src/log.ts
@@ -1,65 +1,84 @@
-import * as fs from 'fs';
-import { fn } from './factory';
-import { GeoEntry, LogEntry, PackageJson } from './types';
+import * as fs from 'fs/promises'
+import { fn } from './factory'
+import { GeoEntry, LogEntry, PackageJson } from './types'
 
-const log = fn<{ fingerprint: string, machine: LogEntry["machine"], message: string, tags: string[], url: string }, void>(
-    async (args, _, { req, res }) => {
-        let response;
-        if (req.ip === '127.0.0.1') {
-            response = await fetch(`http://ip-api.com/json/8.8.4.4?fields=status,countryCode,lat,lon`);
-        }
-        else {
-            response = await fetch(`http://ip-api.com/json/${req.ip}?fields=status,countryCode,lat,lon`);
-        }
-        const sourceGeo: GeoEntry = await response.json();
+const log = fn<
+  {
+    fingerprint: string
+    machine: LogEntry['machine']
+    message: string
+    tags: string[]
+    url: string
+  },
+  void
+>(
+  async (args, _, {req, res}) => {
+    const sourceGeo: GeoEntry = await getGeo(req.ip)
+    const destinationGeo: GeoEntry = await getGeo(req.hostname)
 
-        if (req.hostname === 'localhost') {
-            response = await fetch(`http://ip-api.com/json/8.8.8.8?fields=status,countryCode,lat,lon`);
-        }
-        else {
-            response = await fetch(`http://ip-api.com/json/${req.hostname}?fields=status,countryCode,lat,lon`);
-        }
-        const destinationGeo: GeoEntry = await response.json();
+    try {
+      const pjson: PackageJson = await fs
+        .readFile('./package.json', 'utf-8')
+        .then(async res => {
+          const result: PackageJson = await JSON.parse(res)
+          return result
+        })
 
-        try {
-            const pjson: PackageJson = await JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
-            const logData: LogEntry = {
-                timestamp: new Date().toISOString(),
-                fingerprint: args.fingerprint,
-                user_agent: req.get('user-agent'),
-                bytes: parseInt(req.get('content-length')),
-                clientIp: req.ip,
-                geo: {
-                    src: sourceGeo.countryCode,
-                    dest: destinationGeo.countryCode,
-                    srcDest: sourceGeo.countryCode + destinationGeo.countryCode,
-                    coordinates: {
-                        lat: sourceGeo.lat,
-                        lon: sourceGeo.lon
-                    }
-                },
-                hostname: req.hostname,
-                machine: args.machine,
-                message: args.message,
-                referer: req.get('referrer'),
-                request: req.path,
-                response: res.statusCode,
-                tags: args.tags,
-                url: args.url,
-                function: pjson.name + ": " + pjson.version
-            };
+      const logData: LogEntry = {
+        timestamp: new Date().toISOString(),
+        fingerprint: args.fingerprint,
+        user_agent: req.get('user-agegggnt') ?? 'default_value',
+        bytes: parseInt(req.get('content-length')) ?? 0,
+        clientIp: req.ip,
+        geo: {
+          src: sourceGeo.countryCode,
+          dest: destinationGeo.countryCode,
+          srcDest: sourceGeo.countryCode + destinationGeo.countryCode,
+          coordinates: {
+            lat: sourceGeo.lat,
+            lon: sourceGeo.lon
+          }
+        },
+        hostname: req.hostname,
+        machine: args.machine,
+        message: args.message,
+        referer: req.get('referrer') ?? 'default_value',
+        request: req.path,
+        response: res.statusCode,
+        tags: args.tags,
+        url: args.url,
+        function: pjson.name + ': ' + pjson.version
+      }
 
-            fs.appendFileSync('./default.log', JSON.stringify(logData) + '\n');
+      await fs.appendFile('./default.log', JSON.stringify(logData) + '\n')
 
-            return 'Success';
-        }
-        catch (e) {
-            return e;
-        }
-    },
-    {
-        name: 'log'
+      return 'Success'
+    } catch (e) {
+      return e
     }
+  },
+  {
+    name: 'log'
+  }
 )
+
+async function getGeo(ip) {
+  let geo: GeoEntry = {
+    countryCode: 'FAIL',
+    lat: 99,
+    lon: 99
+  }
+
+  await fetch(
+    `http://ip-api.com/json/${ip}?fields=status,countryCode,lat,lon`
+  ).then(async res => {
+    let response = await res.json()
+    if (response.status !== 'fail') {
+      geo = {...response}
+    }
+  })
+
+  return geo
+}
 
 export default log
