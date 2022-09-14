5241dd0d99cccff620519542bd308b6d71624dc6
Pinterics David <55298934+pinterid@users.noreply.github.com>
2022-09-14 12:04:47 +0200

    feat: add log endpoint (#1)
    * Add log function
    
    The log functionallity of the prototyp ehas been added.
    
    * Aplly requested changes
    
    The logs are no longer created using Python, the functionallity shifted to TypeScript.
    
    * Add error handling
    
    Error handling was added and the function value was added to log
    
    * Prettify Code
    
    The code has been cleaned up.
    
    * Remove typo
    
    A typo has been removed
    
    * Improve code
    
    The error handling has been upgraded and helper functions have been introduced.
    
    * Apply suggestions from @schettn
    
    Co-authored-by: Nico Schett <52858351+schettn@users.noreply.github.com>
    
    * Apply requested changes from @schettn
    
    Co-authored-by: Nico Schett <52858351+schettn@users.noreply.github.com>

--- src/factory.ts
+++ src/factory.ts
@@ -1,5 +1,11 @@
-import {makeFn} from '@snek-at/functions'
+import { makeFn } from '@snek-at/functions'
+
+export const url = process.env.IS_OFFLINE
+  ? process.env.CODESPACE_NAME
+    ? `https://${process.env.CODESPACE_NAME}-5000.githubpreview.dev/graphql`
+    : 'http://localhost:4000/graphql'
+  : process.env.ENDPOINT_URL_LOG || process.env.GATSBY_ENDPOINT_URL_LOG
 
 export const fn = makeFn({
-  url: 'https://<your-snek-functions-url>'
+  url
 })
