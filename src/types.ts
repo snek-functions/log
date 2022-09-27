39d16b4f922dbdff4714583ad0d9bacd1f7b5f6e
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-09-27 12:40:42 +0000

    feat: add sf declarations

--- src/types.ts
+++ src/types.ts
@@ -1,48 +1,48 @@
 export interface LogEntry {
-    "timestamp": string,
-    "fingerprint": string,
-    "user_agent": string,
-    "bytes": number,
-    "clientIp": string,
-    "geo": {
-        "srcDest": string,
-        "src": string,
-        "dest": string,
-        "coordinates": {
-            "lat": number,
-            "lon": number
-        }
-    },
-    "hostname": string,
-    "machine": {
-        "ram": number,
-        "os": string,
-    },
-    "message": string,
-    "referer": string,
-    "request": string,
-    "response": number,
-    "tags": string[],
-    "url": string,
-    "function": string
-};
+  timestamp: string
+  fingerprint: string | null
+  user_agent: string | null
+  bytes: number | null
+  clientIp: string | null
+  geo: {
+    srcDest: string | null
+    src: string | null
+    dest: string | null
+    coordinates: {
+      lat: number | null
+      lon: number | null
+    }
+  }
+  hostname: string | null
+  machine: {
+    ram: number | null
+    os: string | null
+  } | null
+  message: string | null
+  referer: string | null
+  request: string | null
+  response: number | null
+  tags: string[]
+  url: string | null
+  function: string | null
+}
 
 export interface GeoEntry {
-    "countryCode": string,
-    "lat": number,
-    "lon": number
+  countryCode: string
+  lat: number
+  lon: number
 }
 
 export interface PackageJson {
-    "name": string,
-    "version": string,
-    "main": string,
-    "private": boolean,
-    "type": string,
-    "description": string,
-    "author": string,
-    "dependencies": object,
-    "devDependencies": object,
-    "scripts": object,
-    "sfDependencies": object
+  name: string
+  version: string
+  main: string
+  private: boolean
+  type: string
+  description: string
+  author: string
+  dependencies: object
+  devDependencies: object
+  scripts: object
+  sfDependencies: object
 }
